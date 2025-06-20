d787379243740d2742af640e43161ceb9f817138
bang <vanbang@quangich.com>
2025-06-20 14:16:31 +0700

    End of section 9

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,11 +9,15 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router';
 import { router } from './app/router/Routers.tsx';
+import { store, StoreContext } from './lib/stores/store.ts';
 
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <StoreContext.Provider value={store}>
+
+    </StoreContext.Provider>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools></ReactQueryDevtools>
       <RouterProvider router={router} />
