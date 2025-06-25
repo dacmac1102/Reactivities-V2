97c3e86110b1190b25656fe06e17610029a5b23f
bang <vanbang@quangich.com>
2025-06-25 11:02:38 +0700

    End of section 14

--- Application/Activities/DTOs/ActivityDto.cs
+++ Application/Activities/DTOs/ActivityDto.cs
@@ -1,15 +1,18 @@
 using System;
+using Application.Profiles.DTOs;
 
-namespace Domain;
+namespace Application.Activities.DTOs;
 
-public class Activity
+public class ActivityDto
 {
-    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Id { get; set; }
     public required string Title { get; set; }
     public DateTime Date { get; set; }
     public required string Description { get; set; }
     public required string Category { get; set; }
     public bool IsCancelled { get; set; }
+    public required string HostDisplayName { get; set; }
+    public required string HostId { get; set; }
 
     public required string City { get; set; }
     public required string Venue { get; set; }
@@ -17,7 +20,5 @@ public class Activity
     public double Longitude { get; set; }
 
 
-    public ICollection<ActivityAttendee> Attendees { get; set; } = [];
-
-
+    public ICollection<UserProfile> Attendees { get; set; } = [];
 }
