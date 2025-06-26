fe2cef165125ded9efd5d4972df0d071b08da6c7
bang <vanbang@quangich.com>
2025-06-26 22:53:38 +0700

    End of section 17

--- client/src/app/layout/UserMenu.tsx
+++ client/src/app/layout/UserMenu.tsx
@@ -28,7 +28,9 @@ export default function BasicMenu() {
         sx={{ fontSize: '1.1rem' }}
       >
         <Box display='flex' alignItems='center' gap={2}>
-          <Avatar></Avatar>
+          <Avatar
+            src={currentUser?.imageUrl}
+            alt='current user image'></Avatar>
           {currentUser?.displayName}
         </Box>
       </Button>
@@ -49,7 +51,7 @@ export default function BasicMenu() {
           </ListItemIcon>
           <ListItemText>Create Activity</ListItemText>
         </MenuItem>
-        <MenuItem component={Link} to='/profile' onClick={handleClose}>
+        <MenuItem component={Link} to={`/profiles/${currentUser?.id}`} onClick={handleClose}>
           <ListItemIcon>
             <Person />
           </ListItemIcon>
--- client/src/lib/hooks/useAccount.ts
+++ client/src/lib/hooks/useAccount.ts
@@ -2,15 +2,13 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import type { LoginSchema } from "../schemas/loginSchema"
 import agent from "../api/agent"
 import type { User } from "../types";
-import { useLocation, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import type { RegisterSchema } from "../schemas/registerSchema";
 import { toast } from "react-toastify";
 
 export const useAccount = () => {
     const queryClient = useQueryClient();
     const navigate = useNavigate();
-    const location = useLocation();
-
     const loginUser = useMutation({
         mutationFn: async (creds: LoginSchema) => {
             await agent.post('/login?useCookies=true', creds);
@@ -48,8 +46,6 @@ export const useAccount = () => {
             return response.data;
         },
         enabled: !queryClient.getQueryData(['user'])
-            && location.pathname !== '/login'
-            && location.pathname !== '/register'
     })
 
     return {
